09952c2b8c277f0722319cc8a84c355d3382fb88
Markides.Markellos <markellos.markides@gmail.com>
2026-07-05 10:35:17 +0300

    feat: persist appearance settings

---
+++ src/features/appearance/useAppearanceSettings.ts
@@ -0,0 +1,46 @@
+import { useEffect, useMemo } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
+import { studyDatabase } from "../../infrastructure/database/studyDatabase";
+import {
+  APPEARANCE_SETTINGS_KEY,
+  applyAppearanceSettings,
+  defaultAppearanceSettings,
+  parseAppearanceSettings,
+  type AppearanceSettings,
+} from "./appearanceSettings";
+
+export function useAppearanceSettings() {
+  const setting = useLiveQuery(
+    () => studyDatabase.settings.get(APPEARANCE_SETTINGS_KEY),
+    [],
+  );
+  const settings = useMemo(
+    () => parseAppearanceSettings(setting?.value),
+    [setting?.value],
+  );
+
+  useEffect(() => {
+    applyAppearanceSettings(settings);
+  }, [settings]);
+
+  async function updateAppearanceSettings(nextSettings: Partial<AppearanceSettings>) {
+    await studyDatabase.settings.put({
+      key: APPEARANCE_SETTINGS_KEY,
+      value: { ...settings, ...nextSettings },
+    });
+  }
+
+  async function resetAppearanceSettings() {
+    await studyDatabase.settings.put({
+      key: APPEARANCE_SETTINGS_KEY,
+      value: defaultAppearanceSettings,
+    });
+  }
+
+  return {
+    isLoading: setting === undefined,
+    settings,
+    updateAppearanceSettings,
+    resetAppearanceSettings,
+  };
+}
