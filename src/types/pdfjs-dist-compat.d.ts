0e9d1a50b7737d75fc0773f27913ddc37e2a1b88
Markides.Markellos <markellos.markides@gmail.com>
2026-07-08 20:43:04 +0300

    fix: augment pdfjs internal document proxy type

--- src/types/pdfjs-dist-compat.d.ts
+++ src/types/pdfjs-dist-compat.d.ts
@@ -1,6 +1,13 @@
 import "pdfjs-dist";
+import "pdfjs-dist/types/src/display/api";
 
 declare module "pdfjs-dist" {
+  interface PDFDocumentProxy {
+    destroy(): Promise<void>;
+  }
+}
+
+declare module "pdfjs-dist/types/src/display/api" {
   interface PDFDocumentProxy {
     destroy(): Promise<void>;
   }
