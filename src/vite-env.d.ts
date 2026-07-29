b2c6aaae63ef272e4ae0896a0f6e020949c7fd87
Markides.Markellos <markellos.markides@gmail.com>
2026-07-29 22:54:04 +0300

    types: declare Cloud Core build configuration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_CLOUD_CORE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare const __APP_BUILD_ID__: string;
